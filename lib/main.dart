daa867de9b9808586cb973ed6c98bedd110a4f34
Shayne <shayne.chang@zencher.com>
2023-03-21 23:24:44 +0800

    create temp UI, listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:vocab_card/controller/data_controller.dart';
+
+import 'model/vocab.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  List<Vocab> vocabList = [];
+  @override
+  void initState() {
+    // TODO: implement initState
+    DataController().getVocabList().then((value) {
+      setState(() {
+        vocabList = value;
+      });
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        home: Scaffold(
+            body: ListView.builder(
+                itemCount: vocabList.length,
+                itemBuilder: (context, index) {
+                  return vocabList.isEmpty
+                      ? ListTile(
+                          title: Text('loading'),
+                        )
+                      : ListTile(
+                          title: Text(vocabList[index].english),
+                        );
+                })));
   }
 }
