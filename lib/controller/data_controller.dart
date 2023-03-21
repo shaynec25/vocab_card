39e70b0759b52f158363351d71151006650b2567
Shayne <shayne.chang@zencher.com>
2023-03-21 23:24:11 +0800

    create data controller

---
+++ lib/controller/data_controller.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:vocab_card/model/vocab.dart';
+
+class DataController {
+  static Uri url = Uri.https('script.google.com',
+      '/macros/s/AKfycbxAK8BaoNXOI6BuUDFZHWpMtfTpWd-TiVk93cXrHwvMzzQhRkVZ4u3OUvt8fPb5lnxdSg/exec');
+  // static String urlString =
+  //     'https://script.google.com/macros/s/AKfycbxAK8BaoNXOI6BuUDFZHWpMtfTpWd-TiVk93cXrHwvMzzQhRkVZ4u3OUvt8fPb5lnxdSg/exec';
+
+  Future<List<Vocab>> getVocabList() async {
+    return await http.get(url).then((response) {
+      var jsonFeedback = jsonDecode(response.body) as List;
+      return jsonFeedback.map((json) => Vocab.fromJson(json)).toList();
+    });
+  }
+}
